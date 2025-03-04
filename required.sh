7d2eb1e290bc75c9ebb1b0cc08166001fe6960e5
voznyiarsen <43888272+voznyiarsen@users.noreply.github.com>
2025-03-04 17:44:17 +0100

    Add files via upload

--- required.sh
+++ required.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
+## CERTS
+#sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
+#sudo certbot certonly --standalone -d domain.org
+## CERTS END
 git clone https://github.com/hashtopolis/server.git
 cd server
 cp ../.env .
-echo "##\n## Building server image\n##"
+echo -e "###\n### Building server image\n###"
 sudo docker build . -t hashtopolis/backend:latest --platform linux/arm64 --target hashtopolis-server-prod
-echo -e "##\n## Server image built\n##"
+echo -e "###\n### Server image built\n###"
 echo $(sudo docker images)
+echo -e "###\n### You can now run (docker compose up -d)\n###"
