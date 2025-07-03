b7931bb2d352024171c585d161b74af375da28dc
voznyiarsen <43888272+voznyiarsen@users.noreply.github.com>
2025-07-03 15:44:44 +0000

    Fix permissions

--- hashtopolis-container-setup.sh
+++ hashtopolis-container-setup.sh
@@ -4,10 +4,10 @@ SCRIPT_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
 git clone https://github.com/hashtopolis/web-ui.git
 git clone https://github.com/hashtopolis/server.git
 
-echo "### Fixing dogshit compatablility... ###"
+echo "### cp $SCRIPT_DIR/Dockerfile-webui $SCRIPT_DIR/web-ui/Dockerfile"
 cp $SCRIPT_DIR/Dockerfile-webui $SCRIPT_DIR/web-ui/Dockerfile
 
-echo "### Building... ###"
+echo "### Building WEB-UI..."
 cd $SCRIPT_DIR/web-ui
 docker buildx build \
   --platform linux/arm64 \
@@ -15,37 +15,30 @@ docker buildx build \
   --target hashtopolis-web-ui-prod \
   --load \
   .
-echo "### web-ui build done ###"
+echo "### WEB-UI image built"
 
-#
-echo "### Setting up SSL for hashtopolis/server... ###"
+echo "### Copying SSL certificates/configs"
 # Certbot/Self-Signed certs
 cp $SCRIPT_DIR/certs/*.pem $SCRIPT_DIR/server
-
 # SSL Sites configs
 cp $SCRIPT_DIR/sites/* $SCRIPT_DIR/server
 
-# use diffs instead actually
-echo "### Copied Dockerfile ####"
+echo "### cp $SCRIPT_DIR/Dockerfile-server $SCRIPT_DIR/server/Dockerfile"
 cp $SCRIPT_DIR/Dockerfile-server $SCRIPT_DIR/server/Dockerfile
-echo "### Files copied ###"
 
-# Building
+echo "### Building SERVER..."
 cd $SCRIPT_DIR/server
-
 docker buildx build \
   --platform linux/arm64 \
   -t hashtopolis/backend:latest \
   --target hashtopolis-server-prod \
   --load \
   .
-echo "### server build done ###"
+echo "### SERVER image built"
 
-cd $SCRIPT_DIR
-
-echo "### BACKEND ###"
+echo "### BACKEND image:"
 docker images hashtopolis/backend
-echo "### FRONTEND ###"
+echo "### FRONTEND image:"
 docker images hashtopolis/frontend
 # certs here
 
